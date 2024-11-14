a9caa23766c8a33dc20950c394f93423ab6d5cdc
c174 <courso95@students.rowan.edu>
2024-11-13 22:53:29 -0500

    Update Howtouse.m
    For finding all info with just ID number or major.

--- Howtouse.m
+++ Howtouse.m
@@ -1,8 +1,8 @@
-% Search for a specific student by ID Number, e.g., Student 22
+% Search for student by ID Number, e.g., Student 22
 student_id = 22;
 specific_student = db.findStudentByID(student_id);
 
-% Show the student's information when and if found
+% Show the student's info when and if found
 if ~isempty(specific_student)
     disp(['Displaying information for Student ' num2str(student_id) ':']);
     specific_student.displayInfo();
@@ -10,13 +10,13 @@ else
     disp(['Student with ID ' num2str(student_id) ' not found.']);
 end
 
-% Specify the major you want to search for
+% Specify the major you want to look for
 desired_major = 'Mathematics';
 
-% Retrieve students in the specified major
+% Retrieve students in major
 students_in_major = db.getStudentsByMajor(desired_major);
 
-% Check if any students were found and display their information
+% Check if any students were found and show their information
 if ~isempty(students_in_major)
     fprintf('Students in the %s major:\n', desired_major);
     for i = 1:length(students_in_major)
